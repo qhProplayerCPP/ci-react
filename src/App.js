859bc4de1f85510aacbb8f6ee6f36c0692414b97
qhProplayerCPP <quanghuy.1334@gmail.com>
2021-01-15 17:45:06 +0700

    ??

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import { FILTER_LIST } from './components/filter';
-import { Pokemon } from './components/list-pokemon';
-import { pokemon } from './pokemondata';
 
 function App() {
   return (
     <div className="App">
-      <Pokemon data={pokemon} />
-      <div className="container-filter">
-        <FILTER_LIST color="red" type="Fire" />
-        <FILTER_LIST color="blue" type="Water" />
-        <FILTER_LIST color="green" type="Grass" />
-      </div>
+      <p>day la app pokemon</p>
     </div>
   );
 }
